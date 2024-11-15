1e56a2e466fed9d6370a678089fafe89a1768429
Dizerter <164652370+Dizerter@users.noreply.github.com>
2024-11-15 19:45:21 +0200

    prints title and paragraph

--- web scraper.py
+++ web scraper.py
@@ -1,3 +1,14 @@
 from bs4 import BeautifulSoup
-soup = BeautifulSoup("https://edition.cnn.com/world",'html.parser')
-print(soup)
+import requests
+url=requests.get("https://edition.cnn.com/2024/11/13/middleeast/israel-expands-ground-operation-lebanon-intl-latam/index.html")
+soup = BeautifulSoup(url.text,'html.parser')
+
+title = soup.find("h1").text
+paragraphs=[]
+paragraphs.append(soup.find_all("p").text)
+paragraph=""
+for p in soup.find_all("p", href=False):
+    paragraph=paragraph + p.get_text()
+
+print(title)
+print (paragraph)
